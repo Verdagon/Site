29d76b0e4ae8f4e05eff9141db68f322704410b6
Evan Ovadia <verdagon@gmail.com>
2022-10-21 13:38:51 -0400

    Backup

--- build.sh
+++ build.sh
@@ -45,8 +45,18 @@ fi
 
 if [ $TARGET == "vision" ] || [ $TARGET == "all" ] ; then
   echo "Doing vision"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/vision/vision app/vision/vision.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/vision/vision app/vision/vision.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/vision/vision app/vision/vision.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/vision/vision app/vision/vision.vmd
+  if [ $? != 0 ]; then
+    echo "Failed!"
+    exit 1
+  fi
+fi
+
+if [ $TARGET == "vision-overview" ] || [ $TARGET == "all" ] ; then
+  echo "Doing vision"
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/vision/overview app/vision/overview.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/vision/overview app/vision/overview.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -55,8 +65,8 @@ fi
 
 if [ $TARGET == "vision-safety-generational-references" ] || [ $TARGET == "all" ] ; then
   echo "Doing vision-safety-generational-references"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/vision/safety-generational-references app/vision/safety-generational-references.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/vision/safety-generational-references app/vision/safety-generational-references.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/vision/safety-generational-references app/vision/safety-generational-references.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/vision/safety-generational-references app/vision/safety-generational-references.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -65,8 +75,8 @@ fi
 
 if [ $TARGET == "vision-safety-type-stability" ] || [ $TARGET == "all" ] ; then
   echo "Doing vision-safety-type-stability"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/vision/safety-type-stability app/vision/safety-type-stability.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/vision/safety-type-stability app/vision/safety-type-stability.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/vision/safety-type-stability app/vision/safety-type-stability.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/vision/safety-type-stability app/vision/safety-type-stability.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -75,8 +85,8 @@ fi
 
 if [ $TARGET == "vision-speed-single-ownership" ] || [ $TARGET == "all" ] ; then
   echo "Doing vision-speed-single-ownership"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/vision/speed-single-ownership app/vision/speed-single-ownership.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/vision/speed-single-ownership app/vision/speed-single-ownership.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/vision/speed-single-ownership app/vision/speed-single-ownership.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/vision/speed-single-ownership app/vision/speed-single-ownership.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -85,8 +95,8 @@ fi
 
 if [ $TARGET == "comparisons" ] || [ $TARGET == "all" ] ; then
   echo "Doing comparisons"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/comparisons app/comparisons.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/comparisons app/comparisons.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/comparisons app/comparisons.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/comparisons app/comparisons.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -95,8 +105,8 @@ fi
 
 if [ $TARGET == "memory-safe" ] || [ $TARGET == "all" ] ; then
   echo "Doing memory-safe"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/memory-safe app/memory-safe.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/memory-safe app/memory-safe.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/memory-safe app/memory-safe.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/memory-safe app/memory-safe.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -105,8 +115,8 @@ fi
 
 if [ $TARGET == "contributors" ] || [ $TARGET == "all" ] ; then
   echo "Doing contributors"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/contributors app/contributors.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/contributors app/contributors.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/contributors app/contributors.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/contributors app/contributors.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -115,8 +125,8 @@ fi
 
 if [ $TARGET == "download" ] || [ $TARGET == "all" ] ; then
   echo "Doing download"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/download app/download.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/download app/download.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/download app/download.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/download app/download.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -125,8 +135,8 @@ fi
 
 if [ $TARGET == "collections" ] || [ $TARGET == "all" ] ; then
   echo "Doing collections"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/collections app/guide/collections.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/collections app/guide/collections.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/collections app/guide/collections.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/collections app/guide/collections.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -135,8 +145,8 @@ fi
 
 if [ $TARGET == "externs" ] || [ $TARGET == "all" ] ; then
   echo "Doing externs"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/externs app/guide/externs.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/externs app/guide/externs.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/externs app/guide/externs.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/externs app/guide/externs.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -145,8 +155,8 @@ fi
 
 if [ $TARGET == "functions" ] || [ $TARGET == "all" ] ; then
   echo "Doing functions"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/functions app/guide/functions.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/functions app/guide/functions.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/functions app/guide/functions.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/functions app/guide/functions.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -155,8 +165,8 @@ fi
 
 if [ $TARGET == "generics" ] || [ $TARGET == "all" ] ; then
   echo "Doing generics"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/generics app/guide/generics.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/generics app/guide/generics.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/generics app/guide/generics.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/generics app/guide/generics.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -165,8 +175,8 @@ fi
 
 if [ $TARGET == "interfaces" ] || [ $TARGET == "all" ] ; then
   echo "Doing interfaces"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/interfaces app/guide/interfaces.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/interfaces app/guide/interfaces.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/interfaces app/guide/interfaces.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/interfaces app/guide/interfaces.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -175,8 +185,8 @@ fi
 
 if [ $TARGET == "introduction" ] || [ $TARGET == "all" ] ; then
   echo "Doing introduction"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/introduction app/guide/introduction.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/introduction app/guide/introduction.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/introduction app/guide/introduction.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/introduction app/guide/introduction.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -185,8 +195,8 @@ fi
 
 if [ $TARGET == "modules" ] || [ $TARGET == "all" ] ; then
   echo "Doing modules"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/modules app/guide/modules.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/modules app/guide/modules.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/modules app/guide/modules.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/modules app/guide/modules.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -195,8 +205,8 @@ fi
 
 if [ $TARGET == "patterns" ] || [ $TARGET == "all" ] ; then
   echo "Doing patterns"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/patterns app/guide/patterns.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/patterns app/guide/patterns.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/patterns app/guide/patterns.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/patterns app/guide/patterns.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -205,8 +215,8 @@ fi
 
 if [ $TARGET == "references" ] || [ $TARGET == "all" ] ; then
   echo "Doing references"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/references app/guide/references.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/references app/guide/references.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/references app/guide/references.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/references app/guide/references.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -215,8 +225,8 @@ fi
 
 if [ $TARGET == "regions" ] || [ $TARGET == "all" ] ; then
   echo "Doing regions"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/regions app/guide/regions.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/regions app/guide/regions.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/regions app/guide/regions.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/regions app/guide/regions.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -225,8 +235,8 @@ fi
 
 if [ $TARGET == "structs" ] || [ $TARGET == "all" ] ; then
   echo "Doing structs"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/structs app/guide/structs.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/structs app/guide/structs.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/structs app/guide/structs.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/structs app/guide/structs.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -235,8 +245,8 @@ fi
 
 if [ $TARGET == "unsafe" ] || [ $TARGET == "all" ] ; then
   echo "Doing unsafe"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/unsafe app/guide/unsafe.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/guide/unsafe app/guide/unsafe.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/unsafe app/guide/unsafe.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/guide/unsafe app/guide/unsafe.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -245,8 +255,8 @@ fi
 
 if [ $TARGET == "home" ] || [ $TARGET == "all" ] ; then
   echo "Doing home"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/home app/home.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/home app/home.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/home app/home.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/home app/home.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -258,8 +268,8 @@ fi
 
 if [ $TARGET == "contribute" ] || [ $TARGET == "all" ] ; then
   echo "Doing contribute"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/contribute app/contribute.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/contribute app/contribute.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/contribute app/contribute.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/contribute app/contribute.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
@@ -268,23 +278,13 @@ fi
 
 if [ $TARGET == "roadmap" ] || [ $TARGET == "all" ] ; then
   echo "Doing roadmap"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/roadmap app/roadmap/roadmap.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/roadmap app/roadmap/roadmap.vmd
+  echo $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/roadmap app/roadmap/roadmap.vmd
+  eval $VMD_SITE_GEN $MODE --compiler_dir $VALESTROM --out public/roadmap app/roadmap/roadmap.vmd
   if [ $? != 0 ]; then
     echo "Failed!"
     exit 1
   fi
   cp app/roadmap/*.css public
-fi
-
-if [ $TARGET == "project" ] || [ $TARGET == "all" ] ; then
-  echo "Doing project"
-  echo $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/project app/project.vmd
-  eval $VMD_SITE_GEN/build/vmdsitegen $MODE --compiler_dir $VALESTROM --out public/project app/project.vmd
-  if [ $? != 0 ]; then
-    echo "Failed!"
-    exit 1
-  fi
 fi
 
 if [ $MODE == "build" ] ; then
